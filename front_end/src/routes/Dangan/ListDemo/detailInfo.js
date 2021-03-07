13f500d70f923953b17803cc377569fae0a79242
lchaoer <1335717080@qq.com>
2021-03-07 14:02:13 +0800

    list

---
+++ front_end/src/routes/Dangan/ListDemo/detailInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Row, Col, Card, Icon, Radio, Dropdown, Menu, message, Table, Search, Input, List, Drawer } from 'antd'
+import axios from 'axios'
+
+import CustomBreadcrumb from '../../../components/CustomBreadcrumb/index'
+import TypingCard from '../../../components/TypingCard'
+
+export default class detailInfo extends React.Component {
+
+    render(){
+        return (<div>太错了哥儿</div>)
+    }
+}
