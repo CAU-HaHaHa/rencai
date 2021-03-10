6e779a99ae92ceff60db96e6e3df1fa6691bf49b
lchaoer <1335717080@qq.com>
2021-03-10 19:18:21 +0800

    login

--- front_end/src/components/ContentMain/index.js
+++ front_end/src/components/ContentMain/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { withRouter, Switch, Redirect } from 'react-router-dom'
 import LoadableComponent from '../../utils/LoadableComponent'
 import PrivateRoute from '../PrivateRoute'
-
+import { inject, observer } from 'mobx-react/index'
+import { getCookie } from '../../utils/Session'
+import loginRequest from '../../api/loginRequest'
+import axios from "axios";
 
 const Home = LoadableComponent(()=>import('../../routes/Home/index'))  //参数一定要是函数，否则不会懒加载，只会代码拆分
 
@@ -24,6 +27,9 @@ const UploadDemo = LoadableComponent(()=>import('../../routes/Entry/UploadDemo/i
 //new
 const JixiaoAddDemo = LoadableComponent(()=>import('../../routes/Dangan/JixiaoDemo/add'))
 
+//员工页面测试Demo
+const Test1 = LoadableComponent(()=>import('../../routes/Test/Test1/index'))
+const Test2 = LoadableComponent(()=>import('../../routes/Test/Test2/index'))
 
 // //显示组件Demo
 // const CarouselDemo = LoadableComponent(()=>import('../../routes/Display/CarouselDemo/index'))
@@ -32,71 +38,86 @@ const JixiaoAddDemo = LoadableComponent(()=>import('../../routes/Dangan/JixiaoDe
 // const TableDemo = LoadableComponent(()=>import('../../routes/Display/TableDemo/index'))
 // const TabsDemo = LoadableComponent(()=>import('../../routes/Display/TabsDemo/index'))
 
-// //反馈组件Demo
-// const SpinDemo = LoadableComponent(()=>import('../../routes/Feedback/SpinDemo/index'))
-// const ModalDemo = LoadableComponent(()=>import('../../routes/Feedback/ModalDemo/index'))
-// const NotificationDemo = LoadableComponent(()=>import('../../routes/Feedback/NotificationDemo/index'))
-
-// //其它
-// const AnimationDemo = LoadableComponent(()=>import('../../routes/Other/AnimationDemo/index'))
-// const GalleryDemo = LoadableComponent(()=>import('../../routes/Other/GalleryDemo/index'))
-// const DraftDemo = LoadableComponent(()=>import('../../routes/Other/DraftDemo/index'))
-// const ChartDemo = LoadableComponent(()=>import('../../routes/Other/ChartDemo/index'))
-// const LoadingDemo = LoadableComponent(()=>import('../../routes/Other/LoadingDemo/index'))
-// const ErrorPage = LoadableComponent(()=>import('../../routes/Other/ErrorPage/index'))
-// const SpringText = LoadableComponent(()=>import('../../routes/Other/SpringText/index'))
 
 //关于
 const About = LoadableComponent(()=>import('../../routes/About/index'))
 
-@withRouter
+@withRouter @inject('appStore') @observer
 class ContentMain extends React.Component {
+
+  refresh(){
+    ///  请求数据库返回结果
+    let cookie = getCookie();
+    let data = loginRequest(
+      cookie[0], 
+      cookie[1],
+      cookie[2],
+      );
+    axios.all([data]).then(
+      res => {
+      this.props.appStore.toggleLogin(true, 
+        {
+          username: cookie[0], 
+          password: cookie[1], 
+          usertype: cookie[2], 
+          userid: res[0].data.data.hr_id,
+          corporationid: res[0].data.data.corporation_id,
+        });
+      }).catch(
+        ()=>{
+          alert("未知错误")
+        }
+      )
+  }
+
   render () {
-    return (
-      <div style={{padding: 16, position: 'relative'}}>
-        <Switch>
-          <PrivateRoute exact path='/home' component={Home}/>
+    if(this.props.appStore.loginUser.usertype==null){
+      this.refresh();
+    }
+    if(this.props.appStore.loginUser.usertype=="1"){
+      return (
+        <div style={{padding: 16, position: 'relative'}}>
+          <Switch>
+            <PrivateRoute exact path='/homeHr' component={Home}/>
 
-          <PrivateRoute exact path='/home/dangan/list' component={ListDemo}/>
-          <PrivateRoute exact path='/home/dangan/jixiao' component={JixiaoDemo}/>
-          <PrivateRoute exact path='/home/dangan/jiangcheng' component={JiangchengDemo}/>
-          <PrivateRoute exact path='/home/dangan/jiangcheng/view' component={JiangchengviweDemo}/>
+            <PrivateRoute exact path='/home/dangan/list' component={ListDemo}/>
+            <PrivateRoute exact path='/home/dangan/jixiao' component={JixiaoDemo}/>
+            <PrivateRoute exact path='/home/dangan/jiangcheng' component={JiangchengDemo}/>
+            <PrivateRoute exact path='/home/dangan/jiangcheng/view' component={JiangchengviweDemo}/>
 
-          <PrivateRoute exact path='/home/navigation/dropdown' component={DropdownDemo}/>
-          <PrivateRoute exact path='/home/navigation/menu' component={MenuDemo}/>
-          <PrivateRoute exact path='/home/navigation/steps' component={StepsDemo}/>
+            <PrivateRoute exact path='/home/navigation/dropdown' component={DropdownDemo}/>
+            <PrivateRoute exact path='/home/navigation/menu' component={MenuDemo}/>
+            <PrivateRoute exact path='/home/navigation/steps' component={StepsDemo}/>
 
-          <PrivateRoute exact path='/home/entry/form/basic-form' component={FormDemo1}/>
-          <PrivateRoute exact path='/home/entry/form/step-form' component={FormDemo2}/>
-          <PrivateRoute exact path='/home/entry/upload' component={UploadDemo}/>
+            <PrivateRoute exact path='/home/entry/form/basic-form' component={FormDemo1}/>
+            <PrivateRoute exact path='/home/entry/form/step-form' component={FormDemo2}/>
+            <PrivateRoute exact path='/home/entry/upload' component={UploadDemo}/>
 
-          {/* new */}
-          <PrivateRoute exact path='/add/:id' component={JixiaoAddDemo}/>
+            {/* new */}
+            <PrivateRoute exact path='/add/:id' component={JixiaoAddDemo}/>
 
-          {/* <PrivateRoute exact path='/home/display/carousel' component={CarouselDemo}/>
-          <PrivateRoute exact path='/home/display/collapse' component={CollapseDemo}/>
-          <PrivateRoute exact path='/home/display/list' component={ListDemo}/>
-          <PrivateRoute exact path='/home/display/table' component={TableDemo}/>
-          <PrivateRoute exact path='/home/display/tabs' component={TabsDemo}/> */}
+            <PrivateRoute exact path='/home/about' component={About}/>
 
-          {/* <PrivateRoute exact path='/home/feedback/modal' component={ModalDemo}/>
-          <PrivateRoute exact path='/home/feedback/notification' component={NotificationDemo}/>
-          <PrivateRoute exact path='/home/feedback/spin' component={SpinDemo}/> */}
+            <Redirect exact from='/' to='/homeHr' component={Home}/>
+          </Switch>
+        </div>
+      )
+    }
+    else{
+      return (
+        <div style={{padding: 16, position: 'relative'}}>
+          <Switch>
+            <PrivateRoute exact path='/homeStaff' component={Home}/>
 
-          {/* <PrivateRoute exact path='/home/other/animation' component={AnimationDemo}/>
-          <PrivateRoute exact path='/home/other/gallery' component={GalleryDemo}/>
-          <PrivateRoute exact path='/home/other/draft' component={DraftDemo}/>
-          <PrivateRoute exact path='/home/other/chart' component={ChartDemo}/>
-          <PrivateRoute exact path='/home/other/loading' component={LoadingDemo}/>
-          <PrivateRoute exact path='/home/other/404' component={ErrorPage}/>
-          <PrivateRoute exact path='/home/other/springText' component={SpringText}/> */}
+            <PrivateRoute exact path='/home/test/test1' component={Test1}/>
+            <PrivateRoute exact path='/home/test/test2' component={Test2}/>
 
-          <PrivateRoute exact path='/home/about' component={About}/>
+            <Redirect exact from='/' to='/homeStaff' component={Home}/>
+          </Switch>
+        </div>
+      )
 
-          <Redirect exact from='/' to='/home'/>
-        </Switch>
-      </div>
-    )
+    }
   }
 }
 
--- front_end/src/store/appStore.js
+++ front_end/src/store/appStore.js
@@ -3,13 +3,24 @@ import {isAuthenticated,authenticateSuccess,logout} from '../utils/Session'
 
 class AppStore {
   @observable isLogin = !!isAuthenticated()  //利用cookie来判断用户是否登录，避免刷新页面后登录状态丢失
-  @observable users = []  //模拟用户数据库
+  // @observable users = []  //模拟用户数据库
   @observable loginUser = {}  //当前登录用户信息
+  /*  loginUser 具体数据如下：
+    username,  // 用户名
+    password,  // 密码
+    usertype,  // 用户类型（1为hr，2为员工）
+    userid,    // 用户id
+    corporationid, // 公司id
+  */
 
   @action toggleLogin(flag,info={}) {
     this.loginUser = info  //设置登录用户信息
     if (flag) {
-      authenticateSuccess(info.username)
+      authenticateSuccess(
+        info.username,  // 用户名
+        info.password,  // 密码
+        info.usertype,  // 用户类型（1为hr，2为员工）
+      )
       this.isLogin = true
     } else {
       logout()
@@ -17,10 +28,14 @@ class AppStore {
     }
 
   }
-  @action initUsers() {
-    const localUsers = localStorage['users']?JSON.parse(localStorage['users']):[]
-    this.users = [{username: 'admin', password: 'admin'},...localUsers]
-  }
+  // @action initUsers() {
+  //   // const localUsers = localStorage['users']?JSON.parse(localStorage['users']):[]
+  //   // this.users = [{username: 'admin', password: 'admin'},...localUsers]
+  //   this.users = [
+  //     {username: 'hr01', password: '123456'},
+  //     {username: 'staff', password: 'staff'},
+  //   ]
+  // }
 }
 
 export default new AppStore()
