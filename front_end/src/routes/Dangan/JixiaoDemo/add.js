537fa53df8c4f83ae9a70253f12c835d11bcf3c3
lyl1006 <79833046+lyl1006@users.noreply.github.com>
2021-03-17 04:18:00 -0800

    Add files via upload

--- front_end/src/routes/Dangan/JixiaoDemo/add.js
+++ front_end/src/routes/Dangan/JixiaoDemo/add.js
@@ -1,53 +1,70 @@
 import React,{Component} from 'react';
 import CustomBreadcrumb from '../../../components/CustomBreadcrumb/index'
 import axios from 'axios';
-import {  Table } from 'antd';
+import {  Table ,Card,Space,Col,Row} from 'antd';
 import { Input,Button } from 'antd';
 import { DatePicker} from 'antd';
+import { observer, inject } from "mobx-react"
+import { withRouter } from 'react-router-dom'
 import './ui.less'
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
+@withRouter @inject('appStore') @observer
 //开始
 class JixiaoAddDemo extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props.match.params.id)    
+        console.log("didi")
+        console.log(this.props.match.params.id)
+        console.log(this.props.appStore.loginUser.corporationid)      
         this.state = {
             //用于页面传值 
             //thisdata : this.props.match.params.dotData, 
             //thisdata : this.props.location.state,
-            thisdata : this.props.match.params.id,
+            //thisdata : this.props.match.params.id,
             dataSelect : [],
             dataSourcep : [],
-            columns: [{
-                    title: '用户ID',
-                    dataIndex: 'user_id',
-                    key: 'user_id'
-                },{
+            columns: [
+                {
+                    title: "公司ID",
+                    dataIndex: "corporation_id",
+                    key: "corporation_id"
+                },
+                {
+                    title: "用户ID",
+                    dataIndex: "user_id",
+                    key: "user_id"
+                },
+                {
                     title: '评价HR',
                     dataIndex: 'hr_id',
                     key: 'hr_id'
-                },{
+                },
+                {
                     title: '绩效分数',
                     dataIndex: 'value',
                     key: 'value'
-                },{
+                },
+                {
                     title: '部门',
                     dataIndex: 'department',
                     key: 'department',
-                },{
+                },
+                {
                     title: '职位',
                     dataIndex: 'post',
                     key: 'post',
-                },{
+                },
+                {
                     title: '记录时间',
                     dataIndex: 'registerdate',
                     key: 'registerdate',
-                }]
+                }
+            ]
 
         }
-        this.Searchjixiao = this.Searchjixiao.bind(this);
+        //this.Searchjixiao = this.Searchjixiao.bind(this);
         this.handleclickbtn = this.handleclickbtn.bind(this);
     }
 
@@ -56,73 +73,142 @@ class JixiaoAddDemo extends React.Component {
     this.props.history.push({pathname:'/home/dangan/jixiao'});
   }
 
-  Searchjixiao(){
-    // this.setState({
-    //     searchContent: this.props.match.params.id,
-    //   })
-    console.log(3333333)
+  // 获取检索条目
+  inputChange = (event, flag) => {
+    console.log(event, "***", flag)
+    if (flag == "value")
+        this.setState({
+        searchvalue: event.target.value
+    })
+    else
+        alert("Wrong input!")
+        console.log(this.state.searchdepartment)
+    } 
+
+ //点击查询按钮，进行多条件检索
+ Search = (event) => {
     let testdata = []
-    console.log(this.props.match.params.id)  
-    console.log(this.state.dataSourcep)
-    for (const temp of this.state.dataSourcep) 
-    {
-        console.log(6666666)
-        console.log(temp.id)
-        if (temp.user_id == this.props.match.params.id) {
-        //   console.log(temp)
-        //   console.log(this.props.match.params.id)
-        testdata.push({
-            hr_id: temp.hr_id,
-            user_id: temp.user_id,
-            department: temp.department,
-            value: temp.value,
-            post: temp.post,
-            description: temp.description,
-            registerdate:temp.registerdate
-          })
-        }
-    }
-    this.setState({
-        dataSelect: testdata
+    for (const temp of this.state.dataSourcep) {
+      temp.value+=''
+      if (temp.value == this.state.searchvalue || this.state.searchvalue == "") {
+              {
+                console.log(temp)
+                testdata.push({
+                  corporation_id: temp.corporation_id,
+                  user_id: temp.user_id,
+                  hr_id: temp.hr_id,
+                  value: temp.value,
+                  department: temp.department,
+                  post: temp.post,
+                  registerdate:temp.registerdate
+                })
+            }
+      }
+      this.setState({
+        dataselect: testdata
       })
-    console.log(this.state.dataSelect)
-}
+      console.log(this.state.dataselect)
+    }
+  }
+
+   //点击查询按钮，进行多条件检索
+ Search1 = (event) => {
+    let testdata = []
+    for (const temp of this.state.dataSourcep) {
+      this.state.searchvalue*=1
+      if (temp.value <= this.state.searchvalue || this.state.searchvalue == "") {
+              {
+                console.log(temp)
+                testdata.push({
+                  corporation_id: temp.corporation_id,
+                  user_id: temp.user_id,
+                  hr_id: temp.hr_id,
+                  value: temp.value,
+                  department: temp.department,
+                  post: temp.post,
+                  registerdate:temp.registerdate
+                })
+            }
+      }
+      this.setState({
+        dataselect: testdata
+      })
+      console.log(this.state.dataselect)
+    }
+  }
+
 
    //api
-    componentDidMount() {
+    componentDidMount() 
+    {
         const _this = this;
-        //https://60418bef7f50e000173aa942.mockapi.io/api/vi/dataSourcep
-        axios.get('http://45.76.99.155/dangan/performance/dataSource')
-        .then(function(response) {
+        axios({
+            method:'get',
+            url: 'http://45.76.99.155/dangan/performance/datauserid',
+            params :{
+              corporation_id: this.props.appStore.loginUser.corporationid,
+              user_id: this.props.match.params.id
+              //corporation_id: 1
+            }
+          }).then(function(response){
+            console.log(response.data)
             _this.setState({
-                dataSourcep: response.data.data,
-                isLoaded: true
+                     dataSourcep: response.data.data,
+                     dataselect: response.data.data,
+                     isLoaded: true
             });
-        });
+          })
     }
 
-    render() {       
+    render() 
+    {       
         const {
             dataSourcep,
             columns
         } = this.state;
         //console.log(44444444)
-        this.state.dataSourcep
-        console.log(this.state.dataSourcep)
         //console.log(55555555)
         return(
             <div>
+                <div>
                 <CustomBreadcrumb arr={['员工档案管理','绩效评价' ,'绩效详情查看']}/>
-                 <Button type="primary" onClick={this.Searchjixiao}>点击查询</Button> 
-                 <Button type="primary" onClick={this.handleclickbtn}>点击返回</Button> 
+                 {/* <Button type="primary" onClick={this.Searchjixiao}>点击查询</Button> 
+                 <Button type="primary" onClick={this.handleclickbtn}>点击返回</Button>  */}
+                 <Card hoverable title="绩效详情查看" className='card-item' block>
+                    <Space direction="middle">
+                        <Row gutter={20}>
+                        <Col span={8}>
+                            <Input addonBefore="绩效:" placeholder=" " onChange={(event) => { this.inputChange(event, "value") }} />     
+                        </Col>
+                        <Col span={5}>
+                            <Button type="primary" onClick={this.Search} block>
+                                精确查询
+                            </Button>
+                        </Col>
+                        <Col span={5}>
+                            <Button type="primary" onClick={this.Search1} block>
+                                范围查询
+                            </Button>
+                        </Col>
+                        <Col span={5}>
+                            <Button type="primary" onClick={this.handleclickbtn} block>
+                                返回上一级
+                            </Button>
+                        </Col>
+                    </Row>
+                    </Space>
+                </Card>
+                </div>
+                <Card hoverable>
                  <Table
                   columns={columns}
                   expandable={{
                     expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
                     rowExpandable: record => record.user_id!== 'Not Expandable',
                     }}
-                  dataSource={this.state.dataSelect}
+                  dataSource={this.state.dataselect}
                  ></Table>  
+                 </Card>
             </div>
         )
     }
